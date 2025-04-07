86724f9d1e915fba966fa86dc54eb1fb1fc2d35c
Timothy Dziedzic <timothydziedzic@yahoo.com>
2025-04-07 12:56:03 -0400

    Added screen saver for loading, fixed footer, fixed displayed birds when switching location.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const birdContainer = document.querySelector('#bird_container');
 const myHeaders = new Headers();
 const eBirdApiToken = '377m29pfd648';
+const footer = document.querySelector('#footer');
 myHeaders.append("X-eBirdApiToken", eBirdApiToken);
 var birdList = new BirdList();
 
@@ -13,14 +14,19 @@ const requestOptions = {
 
 document.querySelector('#back_to_top_button').addEventListener('click', function() {
     window.scrollTo(0, 0);
+    footer.classList.remove("show");
 });
 
 async function fetchEbirdData(locationId) {
+    const loader = document.getElementById("loader");
     try {
+
+        loader.classList.remove("hidden"); // Show "Processing..."
+        //await delay(3000); // Simulate a delay for the loader
         const response = await fetch(`https://api.ebird.org/v2/data/obs/AL/recent?includeProvisional=true&r=${locationId}`, requestOptions);
         const eBirdData = await response.json();
         birdList = new BirdList();
-
+    
         eBirdData.forEach((bird) => {
             birdList.addBird(bird);
         });
@@ -28,7 +34,12 @@ async function fetchEbirdData(locationId) {
         displayBirds(); // Call next function after everything is done
     } catch (error) {
         console.error("Error fetching bird data:", error);
+    }finally {
+        loader.classList.add("hidden"); // Always hide loader, even if there's an error
     }
+}
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
@@ -36,6 +47,7 @@ function displayBirds() {
 birdContainer.innerHTML = '';
   birdContainer.appendChild(birdList.getBirdsGroup());
   birdContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+  footer.classList.add("show");
 }
 
 
@@ -68,6 +80,8 @@ const locationMenu = document.getElementById("location_selection_menu");
 
 function handleLocationChange() {
     const selectedValue = locationMenu.value;
+    birdContainer.innerHTML = '';
+    footer.classList.remove("show");
     load_locations(selectedValue);
 }
 
@@ -81,3 +95,4 @@ function load_locations_in_select() {
 }
 
 locationMenu.addEventListener("change", handleLocationChange);
+
